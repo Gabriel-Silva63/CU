3acfae742706dafe5d83d71b6efe23b684841a16
Nuly <gabsthegamer7@gmail.com>
2024-04-25 16:46:49 -0300

    desisto

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+document.getElementById("cadastro").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+
